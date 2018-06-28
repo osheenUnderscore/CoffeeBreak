645279b0a689dc0cbe48be43539aa0b7f726d969
osheenUnderscore <oisin_mc@hotmail.co.uk>
2018-06-28 22:01:43 +0100

    Upload repo

---
+++ CoffeeBreak/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CoffeeBreak
+{
+    partial class CoffeeBreak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CoffeeBreak));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.shutdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shutdownToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 48);
+            // 
+            // shutdownToolStripMenuItem
+            // 
+            this.shutdownToolStripMenuItem.Name = "shutdownToolStripMenuItem";
+            this.shutdownToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.shutdownToolStripMenuItem.Text = "Shutdown";
+            this.shutdownToolStripMenuItem.Click += new System.EventHandler(this.shutdownToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // CoffeeBreak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "CoffeeBreak";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CoffeeBreak";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem shutdownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ CoffeeBreak/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// custom added
+using System.IO;
+
+
+namespace CoffeeBreak
+{
+    public partial class CoffeeBreak : Form
+    {
+        public CoffeeBreak()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void shutdownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TimeShutdown(1);
+        }
+
+        private void TimeShutdown(int seconds)
+        {
+            // run command, show popup. For testing
+            //string strCmdShutdown;
+            //strCmdShutdown = "/C shutdown -s -t " + seconds;
+            //strCmdShutdown = "/C cd D:\\Programming && dir && type 'hello' > newfile.txt";
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdShutdown);
+
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdShutdown);
+
+            //System.IO.File.WriteAllText(@"D:\\Programming\\newfile.txt", "text");
+
+            // print to file. For testing
+            //string path = @"D:\Programming\newfile.txt";
+            //FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                StreamWriter textOut = new StreamWriter("D:\\Programming\\newfile.txt");
+                textOut.WriteLine("hello");
+                textOut.Close();
+            }
+            catch(FileNotFoundException)
+            {
+                MessageBox.Show("filepath not found.",
+                "File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("dir not found.",
+                "Directory Not Found");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException");
+            }            finally
+            {
+                System.Diagnostics.Process.Start("CMD.exe", "/C echo DONE!!!!!");
+            }
+            //StreamWriter textOut = new StreamWriter("D:\\Programming\\newfile.txt");
+            //textOut.WriteLine("hello");
+            //textOut.Close();
+            //System.Diagnostics.Process.Start("CMD.exe", "/C echo DONE!!!!!");
+            //File.WriteAllText(@"D:\Programming\file.txt", strCmdShutdown);
+
+            // sending command to cmd.exe without it popping up
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //startInfo.FileName = "cmd.exe";
+            //startInfo.Arguments = "/C cd D:\\Programming | type hello > newfile.txt";
+            //process.StartInfo = startInfo;
+            //process.Start();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
